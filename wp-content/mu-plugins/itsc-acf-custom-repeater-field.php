9527477673a9eb0316a82788c66b53fd64b075d3
Denis Lapa <info@itsc.ee>
2026-05-11 15:36:05 +0300

    Cases posts visuality refactoring

--- wp-content/mu-plugins/itsc-acf-custom-repeater-field.php
+++ wp-content/mu-plugins/itsc-acf-custom-repeater-field.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * Plugin Name: ITSC ACF Repeater Field
- * Description: Adds a lightweight Repeater field type for ACF.
+ * Plugin Name: ITSC ACF Custom Repeater Field
+ * Description: Adds a lightweight custom_repeater field type for ACF and maps old repeater fields to it.
  * Version: 1.0.0
  * Author: ITSC
  */
@@ -13,25 +13,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 add_action(
 	'acf/include_field_types',
 	function () {
-		if ( ! class_exists( 'acf_field' ) || class_exists( 'acf_field_itsc_repeater' ) ) {
+		if ( ! class_exists( 'acf_field' ) || class_exists( 'acf_field_itsc_custom_repeater' ) ) {
 			return;
 		}
 
-		class acf_field_itsc_repeater extends acf_field {
+		class acf_field_itsc_custom_repeater extends acf_field {
 			public function initialize() {
-				$this->name          = 'repeater';
-				$this->label         = __( 'Repeater', 'acf' );
-				$this->category      = 'layout';
-				$this->description   = __( 'Repeat a set of sub fields.', 'acf' );
-				$this->preview_image = function_exists( 'acf_get_url' ) ? acf_get_url() . '/assets/images/field-type-previews/field-preview-repeater.png' : false;
-				$this->defaults      = array(
+				$this->name        = 'custom_repeater';
+				$this->label       = __( 'Custom Repeater', 'acf' );
+				$this->category    = 'layout';
+				$this->description = __( 'Repeat a set of sub fields.', 'acf' );
+				$this->defaults    = array(
 					'sub_fields'   => array(),
 					'layout'       => 'block',
 					'min'          => 0,
 					'max'          => 0,
 					'button_label' => __( 'Add Row', 'acf' ),
 				);
-				$this->have_rows     = 'multi';
+				$this->have_rows   = 'multi';
 
 				$this->add_field_filter( 'acf/prepare_field_for_export', array( $this, 'prepare_field_for_export' ) );
 				$this->add_field_filter( 'acf/prepare_field_for_import', array( $this, 'prepare_field_for_import' ) );
@@ -60,8 +59,8 @@ add_action(
 				for ( $i = 0; $i < $total; $i++ ) {
 					$row = array();
 					foreach ( $field['sub_fields'] as $sub_field ) {
-						$sub_field          = $this->prepare_sub_field_for_db( $sub_field, $field, $i );
-						$row[ $sub_field['key'] ] = acf_get_value( $post_id, $sub_field );
+						$sub_field               = $this->prepare_sub_field_for_db( $sub_field, $field, $i );
+						$row[ $sub_field['key'] ] = get_post_meta( $post_id, $sub_field['name'], true );
 					}
 					$rows[] = $row;
 				}
@@ -79,13 +78,19 @@ add_action(
 
 				foreach ( $rows as $i => $row ) {
 					foreach ( $field['sub_fields'] as $sub_field ) {
-						$input_key = $sub_field['key'];
-						if ( ! array_key_exists( $input_key, $row ) ) {
+						$input_key  = $sub_field['key'];
+						$input_name = isset( $sub_field['_name'] ) ? $sub_field['_name'] : $sub_field['name'];
+
+						if ( array_key_exists( $input_key, $row ) ) {
+							$input_value = $row[ $input_key ];
+						} elseif ( array_key_exists( $input_name, $row ) ) {
+							$input_value = $row[ $input_name ];
+						} else {
 							continue;
 						}
 
 						$prepared_sub_field = $this->prepare_sub_field_for_db( $sub_field, $field, $i );
-						acf_update_value( $row[ $input_key ], $post_id, $prepared_sub_field );
+						acf_update_value( $input_value, $post_id, $prepared_sub_field );
 					}
 				}
 
@@ -105,10 +110,11 @@ add_action(
 					return false;
 				}
 
-				foreach ( $value as &$row ) {
+				foreach ( $value as $i => &$row ) {
 					foreach ( $field['sub_fields'] as $sub_field ) {
-						$sub_value = $row[ $sub_field['key'] ] ?? null;
-						$name      = $sub_field['_name'] ?? $sub_field['name'];
+						$sub_field = $this->prepare_sub_field_for_db( $sub_field, $field, $i );
+						$sub_value = isset( $row[ $sub_field['key'] ] ) ? $row[ $sub_field['key'] ] : null;
+						$name      = isset( $sub_field['_name'] ) ? $sub_field['_name'] : $sub_field['name'];
 
 						$row[ $name ] = acf_format_value( $sub_value, $post_id, $sub_field, $escape_html );
 						unset( $row[ $sub_field['key'] ] );
@@ -125,8 +131,8 @@ add_action(
 
 				$rows  = is_array( $value ) ? array_values( array_filter( $value, 'is_array' ) ) : array();
 				$count = count( $rows );
-				$min   = absint( $field['min'] ?? 0 );
-				$max   = absint( $field['max'] ?? 0 );
+				$min   = absint( isset( $field['min'] ) ? $field['min'] : 0 );
+				$max   = absint( isset( $field['max'] ) ? $field['max'] : 0 );
 
 				if ( $min && $count < $min ) {
 					return sprintf( __( 'Minimum rows not reached (%d rows)', 'acf' ), $min );
@@ -155,15 +161,14 @@ add_action(
 				}
 
 				$rows = is_array( $field['value'] ) ? array_values( $field['value'] ) : array();
-				$min  = absint( $field['min'] ?? 0 );
+				$min  = absint( isset( $field['min'] ) ? $field['min'] : 0 );
 				while ( count( $rows ) < $min ) {
 					$rows[] = array();
 				}
 
-				$layout = in_array( $field['layout'], array( 'block', 'row', 'table' ), true ) ? $field['layout'] : 'block';
 				?>
-				<div class="itsc-acf-repeater -<?php echo esc_attr( $layout ); ?>" data-min="<?php echo esc_attr( absint( $field['min'] ?? 0 ) ); ?>" data-max="<?php echo esc_attr( absint( $field['max'] ?? 0 ) ); ?>">
-					<div class="itsc-acf-repeater-rows">
+				<div class="itsc-acf-custom-repeater" data-min="<?php echo esc_attr( absint( isset( $field['min'] ) ? $field['min'] : 0 ) ); ?>" data-max="<?php echo esc_attr( absint( isset( $field['max'] ) ? $field['max'] : 0 ) ); ?>">
+					<div class="itsc-acf-custom-repeater-rows">
 						<?php
 						foreach ( $rows as $i => $row ) {
 							$this->render_row( $field, $row, $i );
@@ -172,19 +177,19 @@ add_action(
 						?>
 					</div>
 					<p class="acf-actions">
-						<a href="#" class="button button-primary itsc-acf-repeater-add-row"><?php echo esc_html( $field['button_label'] ?: __( 'Add Row', 'acf' ) ); ?></a>
+						<a href="#" class="button button-primary itsc-acf-custom-repeater-add-row"><?php echo esc_html( ! empty( $field['button_label'] ) ? $field['button_label'] : __( 'Add Row', 'acf' ) ); ?></a>
 					</p>
 				</div>
 				<?php
 			}
 
 			private function render_row( $field, $row, $index, $is_clone = false ) {
-				$row_class = $is_clone ? ' itsc-acf-repeater-clone acf-clone' : '';
+				$row_class = $is_clone ? ' itsc-acf-custom-repeater-clone acf-clone' : '';
 				?>
-				<div class="itsc-acf-repeater-row<?php echo esc_attr( $row_class ); ?>" data-index="<?php echo esc_attr( $index ); ?>">
-					<div class="itsc-acf-repeater-row-toolbar">
-						<span class="itsc-acf-repeater-row-title"><?php echo esc_html__( 'Row', 'acf' ); ?> <span class="itsc-acf-repeater-row-number"></span></span>
-						<a href="#" class="acf-icon -minus small itsc-acf-repeater-remove-row" title="<?php esc_attr_e( 'Remove row', 'acf' ); ?>"></a>
+				<div class="itsc-acf-custom-repeater-row<?php echo esc_attr( $row_class ); ?>" data-index="<?php echo esc_attr( $index ); ?>">
+					<div class="itsc-acf-custom-repeater-row-toolbar">
+						<span class="itsc-acf-custom-repeater-row-title"><?php echo esc_html__( 'Row', 'acf' ); ?> <span class="itsc-acf-custom-repeater-row-number"></span></span>
+						<a href="#" class="acf-icon -minus small itsc-acf-custom-repeater-remove-row" title="<?php esc_attr_e( 'Remove row', 'acf' ); ?>"></a>
 					</div>
 					<div class="acf-fields -top -border">
 						<?php
@@ -199,7 +204,7 @@ add_action(
 			}
 
 			private function prepare_sub_field_for_render( $sub_field, $field, $row, $index ) {
-				$sub_field['value']  = $row[ $sub_field['key'] ] ?? ( $sub_field['default_value'] ?? null );
+				$sub_field['value']  = isset( $row[ $sub_field['key'] ] ) ? $row[ $sub_field['key'] ] : ( isset( $sub_field['default_value'] ) ? $sub_field['default_value'] : null );
 				$sub_field['prefix'] = $field['name'] . '[' . $index . ']';
 
 				if ( ! empty( $field['required'] ) ) {
@@ -210,7 +215,7 @@ add_action(
 			}
 
 			private function prepare_sub_field_for_db( $sub_field, $field, $index ) {
-				$sub_name           = $sub_field['_name'] ?? $sub_field['name'];
+				$sub_name           = isset( $sub_field['_name'] ) ? $sub_field['_name'] : $sub_field['name'];
 				$sub_field['_name'] = $sub_name;
 				$sub_field['name']  = $field['name'] . '_' . $index . '_' . $sub_name;
 
@@ -286,29 +291,22 @@ add_action(
 			}
 		}
 
-		acf_register_field_type( 'acf_field_itsc_repeater' );
+		acf_register_field_type( 'acf_field_itsc_custom_repeater' );
 	}
 );
 
-add_action(
-	'acf/field_group/admin_footer',
-	function () {
-		?>
-		<script>
-			(function() {
-				if (!window.acf || !acf.get || !acf.set) {
-					return;
-				}
+add_filter(
+	'acf/load_field',
+	function ( $field ) {
+		$is_acf_pro = function_exists( 'acf_is_pro' ) && acf_is_pro();
 
-				var proFieldTypes = acf.get('PROFieldTypes') || {};
-				if (proFieldTypes.repeater) {
-					delete proFieldTypes.repeater;
-					acf.set('PROFieldTypes', proFieldTypes);
-				}
-			})();
-		</script>
-		<?php
-	}
+		if ( isset( $field['type'] ) && 'repeater' === $field['type'] && ! $is_acf_pro ) {
+			$field['type'] = 'custom_repeater';
+		}
+
+		return $field;
+	},
+	1
 );
 
 add_action(
@@ -316,11 +314,11 @@ add_action(
 	function () {
 		?>
 		<style>
-			.itsc-acf-repeater-row {
+			.itsc-acf-custom-repeater-row {
 				margin: 0 0 12px;
 				background: #fff;
 			}
-			.itsc-acf-repeater-row-toolbar {
+			.itsc-acf-custom-repeater-row-toolbar {
 				display: flex;
 				align-items: center;
 				justify-content: space-between;
@@ -329,35 +327,35 @@ add_action(
 				border-bottom: 0;
 				background: #f6f7f7;
 			}
-			.itsc-acf-repeater-row-title {
+			.itsc-acf-custom-repeater-row-title {
 				font-weight: 600;
 			}
-			.itsc-acf-repeater-clone {
+			.itsc-acf-custom-repeater-clone {
 				display: none !important;
 			}
 		</style>
 		<script>
 			(function($) {
 				function renumber($repeater) {
-					var $rows = $repeater.find('> .itsc-acf-repeater-rows > .itsc-acf-repeater-row').not('.itsc-acf-repeater-clone');
+					var $rows = $repeater.find('> .itsc-acf-custom-repeater-rows > .itsc-acf-custom-repeater-row').not('.itsc-acf-custom-repeater-clone');
 					var max = parseInt($repeater.data('max'), 10) || 0;
 
 					$rows.each(function(index) {
-						$(this).attr('data-index', index).find('.itsc-acf-repeater-row-number').text(index + 1);
+						$(this).attr('data-index', index).find('.itsc-acf-custom-repeater-row-number').text(index + 1);
 					});
 
-					$repeater.find('> .acf-actions .itsc-acf-repeater-add-row').toggle(!max || $rows.length < max);
+					$repeater.find('> .acf-actions .itsc-acf-custom-repeater-add-row').toggle(!max || $rows.length < max);
 				}
 
 				function replaceIndex(value, index) {
 					return value.split('acfcloneindex').join(index);
 				}
 
-				$(document).on('click', '.itsc-acf-repeater-add-row', function(e) {
+				$(document).on('click', '.itsc-acf-custom-repeater-add-row', function(e) {
 					e.preventDefault();
 
-					var $repeater = $(this).closest('.itsc-acf-repeater');
-					var $rows = $repeater.find('> .itsc-acf-repeater-rows > .itsc-acf-repeater-row').not('.itsc-acf-repeater-clone');
+					var $repeater = $(this).closest('.itsc-acf-custom-repeater');
+					var $rows = $repeater.find('> .itsc-acf-custom-repeater-rows > .itsc-acf-custom-repeater-row').not('.itsc-acf-custom-repeater-clone');
 					var max = parseInt($repeater.data('max'), 10) || 0;
 
 					if (max && $rows.length >= max) {
@@ -365,9 +363,9 @@ add_action(
 					}
 
 					var index = $rows.length;
-					var $clone = $repeater.find('> .itsc-acf-repeater-rows > .itsc-acf-repeater-clone').first();
+					var $clone = $repeater.find('> .itsc-acf-custom-repeater-rows > .itsc-acf-custom-repeater-clone').first();
 					var html = replaceIndex($clone.prop('outerHTML'), index);
-					var $row = $(html).removeClass('itsc-acf-repeater-clone acf-clone').show();
+					var $row = $(html).removeClass('itsc-acf-custom-repeater-clone acf-clone').show();
 
 					$row.insertBefore($clone);
 					if (window.acf) {
@@ -376,23 +374,23 @@ add_action(
 					renumber($repeater);
 				});
 
-				$(document).on('click', '.itsc-acf-repeater-remove-row', function(e) {
+				$(document).on('click', '.itsc-acf-custom-repeater-remove-row', function(e) {
 					e.preventDefault();
 
-					var $repeater = $(this).closest('.itsc-acf-repeater');
+					var $repeater = $(this).closest('.itsc-acf-custom-repeater');
 					var min = parseInt($repeater.data('min'), 10) || 0;
-					var $rows = $repeater.find('> .itsc-acf-repeater-rows > .itsc-acf-repeater-row').not('.itsc-acf-repeater-clone');
+					var $rows = $repeater.find('> .itsc-acf-custom-repeater-rows > .itsc-acf-custom-repeater-row').not('.itsc-acf-custom-repeater-clone');
 
 					if ($rows.length <= min) {
 						return;
 					}
 
-					$(this).closest('.itsc-acf-repeater-row').remove();
+					$(this).closest('.itsc-acf-custom-repeater-row').remove();
 					renumber($repeater);
 				});
 
 				$(function() {
-					$('.itsc-acf-repeater').each(function() {
+					$('.itsc-acf-custom-repeater').each(function() {
 						renumber($(this));
 					});
 				});
---
+++ wp-content/mu-plugins/itsc-case-acf-template.php
@@ -0,0 +1,362 @@
+<?php
+/**
+ * Plugin Name: ITSC Case ACF Template
+ * Description: Renders single case posts from ACF fields.
+ * Version: 1.0.0
+ * Author: ITSC
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+function itsc_is_case_post( $post_id = 0 ) {
+	$post_id = $post_id ?: get_the_ID();
+
+	return $post_id && is_singular( 'post' ) && has_category( 'cases', $post_id );
+}
+
+function itsc_case_get_field( $field, $post_id ) {
+	if ( ! function_exists( 'get_field' ) ) {
+		return null;
+	}
+
+	return get_field( $field, $post_id );
+}
+
+function itsc_case_get_repeater_items( $field, $post_id, $sub_field = 'item' ) {
+	$rows = itsc_case_get_field( $field, $post_id );
+	if ( empty( $rows ) || ! is_array( $rows ) ) {
+		return array();
+	}
+
+	$items = array();
+	foreach ( $rows as $row ) {
+		if ( is_array( $row ) && isset( $row[ $sub_field ] ) && '' !== $row[ $sub_field ] ) {
+			$items[] = (string) $row[ $sub_field ];
+		}
+	}
+
+	return $items;
+}
+
+function itsc_case_render_list( $items, $class = '' ) {
+	if ( ! $items ) {
+		return '';
+	}
+
+	$html = '<ul class="itsc-case-list ' . esc_attr( $class ) . '">';
+	foreach ( $items as $item ) {
+		$html .= '<li>' . esc_html( $item ) . '</li>';
+	}
+	$html .= '</ul>';
+
+	return $html;
+}
+
+function itsc_case_render_template( $post_id ) {
+	$title                 = get_the_title( $post_id );
+	$short_description     = itsc_case_get_field( 'short_description', $post_id );
+	$what_was_implemented = itsc_case_get_repeater_items( 'what_was_implemented', $post_id );
+	$results               = itsc_case_get_repeater_items( 'results', $post_id );
+	$stack                 = itsc_case_get_repeater_items( 'stack', $post_id );
+	$highlight             = itsc_case_get_repeater_items( 'highlight', $post_id );
+	$screenshot            = itsc_case_get_field( 'architecture_screenshot', $post_id );
+	$screenshot_url        = is_array( $screenshot ) && ! empty( $screenshot['url'] ) ? $screenshot['url'] : '';
+	$screenshot_preview    = is_array( $screenshot ) && ! empty( $screenshot['sizes']['large'] ) ? $screenshot['sizes']['large'] : $screenshot_url;
+	$screenshot_alt        = is_array( $screenshot ) && ! empty( $screenshot['alt'] ) ? $screenshot['alt'] : $title;
+	$screenshot_lightbox   = 'itsc-case-screenshot-' . absint( $post_id );
+
+	ob_start();
+	?>
+	<article class="itsc-case">
+		<header class="itsc-case-hero">
+			<?php if ( $highlight ) : ?>
+				<div class="itsc-case-eyebrow"><?php echo esc_html( implode( ' / ', $highlight ) ); ?></div>
+			<?php endif; ?>
+			<h2><?php echo esc_html( $title ); ?></h2>
+			<?php if ( $short_description ) : ?>
+				<p class="itsc-case-summary"><?php echo esc_html( $short_description ); ?></p>
+			<?php endif; ?>
+			<?php if ( $stack ) : ?>
+				<div class="itsc-case-stack">
+					<span class="itsc-case-stack-label"><?php esc_html_e( 'Stack:', 'itsc' ); ?></span>
+					<div class="itsc-case-stack-tags">
+						<?php foreach ( $stack as $stack_item ) : ?>
+							<span class="itsc-case-stack-tag"><?php echo esc_html( $stack_item ); ?></span>
+						<?php endforeach; ?>
+					</div>
+				</div>
+			<?php endif; ?>
+
+			<?php if ( $screenshot_url ) : ?>
+				<figure class="itsc-case-screenshot-preview">
+					<a href="#<?php echo esc_attr( $screenshot_lightbox ); ?>" aria-label="<?php esc_attr_e( 'Open architecture preview', 'itsc' ); ?>">
+						<img src="<?php echo esc_url( $screenshot_preview ); ?>" alt="<?php echo esc_attr( $screenshot_alt ); ?>">
+					</a>
+				</figure>
+				<div id="<?php echo esc_attr( $screenshot_lightbox ); ?>" class="itsc-case-lightbox" aria-hidden="true">
+					<a class="itsc-case-lightbox-backdrop" href="#" aria-label="<?php esc_attr_e( 'Close architecture preview', 'itsc' ); ?>"></a>
+					<figure class="itsc-case-lightbox-content">
+						<a class="itsc-case-lightbox-close" href="#" aria-label="<?php esc_attr_e( 'Close architecture preview', 'itsc' ); ?>">×</a>
+						<img src="<?php echo esc_url( $screenshot_url ); ?>" alt="<?php echo esc_attr( $screenshot_alt ); ?>">
+					</figure>
+				</div>
+			<?php endif; ?>
+		</header>
+
+		<div class="itsc-case-grid">
+			<?php if ( $what_was_implemented ) : ?>
+				<section class="itsc-case-section">
+					<h3><?php esc_html_e( 'What was implemented', 'itsc' ); ?></h3>
+					<?php echo itsc_case_render_list( $what_was_implemented, 'itsc-case-list-implemented' ); ?>
+				</section>
+			<?php endif; ?>
+
+			<?php if ( $results ) : ?>
+				<section class="itsc-case-section">
+					<h3><?php esc_html_e( 'Result', 'itsc' ); ?></h3>
+					<?php echo itsc_case_render_list( $results, 'itsc-case-list-results' ); ?>
+				</section>
+			<?php endif; ?>
+		</div>
+
+	</article>
+	<?php
+
+	return ob_get_clean();
+}
+
+add_filter(
+	'the_content',
+	function ( $content ) {
+		if ( is_admin() || ! in_the_loop() || ! is_main_query() || ! itsc_is_case_post() ) {
+			return $content;
+		}
+
+		return itsc_case_render_template( get_the_ID() );
+	},
+	30
+);
+
+add_filter(
+	'astra_single_post_navigation_enabled',
+	function ( $enabled ) {
+		if ( itsc_is_case_post( get_queried_object_id() ) ) {
+			return false;
+		}
+
+		return $enabled;
+	}
+);
+
+add_filter(
+	'astra_single_post_meta',
+	function ( $meta ) {
+		if ( itsc_is_case_post( get_queried_object_id() ) ) {
+			return '';
+		}
+
+		return $meta;
+	}
+);
+
+add_action(
+	'wp_head',
+	function () {
+		if ( ! itsc_is_case_post( get_queried_object_id() ) ) {
+			return;
+		}
+		?>
+		<style id="itsc-case-acf-template-css">
+			body.single-post.category-cases .entry-header,
+			body.single-post.category-cases .entry-meta,
+			body.single-post.category-cases .ast-breadcrumbs-wrapper,
+			body.single-post.category-cases .post-navigation,
+			body.single-post.category-cases .ast-single-post-order,
+			body.single-post.category-cases .ast-single-post-order + .entry-meta {
+				display: none !important;
+			}
+			.itsc-case {
+				max-width: 1120px;
+				margin: 0 auto;
+				padding: 72px 20px 88px;
+			}
+			.itsc-case-hero {
+				max-width: 880px;
+				margin-bottom: 44px;
+			}
+			.itsc-case-eyebrow {
+				margin-bottom: 14px;
+				color: var(--ast-global-color-2);
+				font-size: 14px;
+				font-weight: 700;
+				text-transform: uppercase;
+			}
+			.itsc-case h2 {
+				margin: 0 0 20px;
+				font-size: clamp(32px, 5vw, 52px);
+				line-height: 1.15;
+			}
+			.itsc-case-summary {
+				max-width: 780px;
+				font-size: 20px;
+				line-height: 1.65;
+			}
+			.itsc-case-stack {
+				margin-top: 26px;
+				padding: 18px 0;
+				border-top: 1px solid var(--ast-border-color);
+				border-bottom: 1px solid var(--ast-border-color);
+				line-height: 1.7;
+			}
+			.itsc-case-stack-label {
+				display: block;
+				margin-bottom: 12px;
+				font-weight: 700;
+			}
+			.itsc-case-stack-tags {
+				display: flex;
+				flex-wrap: wrap;
+				gap: 8px;
+			}
+			.itsc-case-stack-tag {
+				display: inline-flex;
+				align-items: center;
+				min-height: 28px;
+				padding: 3px 10px;
+				border: 1px solid var(--ast-border-color);
+				background: var(--ast-global-color-4);
+				border-radius: 999px;
+				font-size: 13px;
+				font-weight: 600;
+				line-height: 1.3;
+				transition: background-color 160ms ease, border-color 160ms ease;
+			}
+			.itsc-case-stack-tag:hover {
+				border-color: var(--ast-global-color-2);
+				background: var(--ast-global-color-5);
+			}
+			.itsc-case-screenshot-preview {
+				max-width: 500px;
+				margin: 28px auto 0;
+			}
+			.itsc-case-screenshot-preview a {
+				display: block;
+				cursor: zoom-in;
+			}
+			.itsc-case-screenshot-preview img {
+				display: block;
+				width: 100%;
+				height: auto;
+				max-height: 360px;
+				object-fit: cover;
+				object-position: top center;
+				border: 1px solid var(--ast-border-color);
+				background: var(--ast-global-color-5);
+				box-shadow: 0 12px 30px rgba(15, 23, 42, 0.08);
+				transition: border-color 160ms ease, box-shadow 160ms ease;
+			}
+			.itsc-case-screenshot-preview a:hover img {
+				border-color: var(--ast-global-color-2);
+				box-shadow: 0 16px 36px rgba(15, 23, 42, 0.12);
+			}
+			.itsc-case-lightbox {
+				position: fixed;
+				inset: 0;
+				z-index: 99999;
+				display: none;
+				padding: 32px;
+			}
+			.itsc-case-lightbox:target {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+			.itsc-case-lightbox-backdrop {
+				position: absolute;
+				inset: 0;
+				background: rgba(15, 23, 42, 0.82);
+			}
+			.itsc-case-lightbox-content {
+				position: relative;
+				z-index: 1;
+				width: min(1180px, 100%);
+				max-height: calc(100vh - 64px);
+				margin: 0;
+				background: var(--ast-global-color-5);
+				box-shadow: 0 24px 70px rgba(0, 0, 0, 0.35);
+			}
+			.itsc-case-lightbox-content img {
+				display: block;
+				width: 100%;
+				max-height: calc(100vh - 64px);
+				object-fit: contain;
+			}
+			.itsc-case-lightbox-close {
+				position: absolute;
+				top: 10px;
+				right: 10px;
+				z-index: 2;
+				display: inline-flex;
+				align-items: center;
+				justify-content: center;
+				width: 34px;
+				height: 34px;
+				border-radius: 50%;
+				background: rgba(255, 255, 255, 0.92);
+				color: var(--ast-global-color-2);
+				font-size: 26px;
+				line-height: 1;
+				text-decoration: none;
+			}
+			.itsc-case-grid {
+				display: grid;
+				grid-template-columns: repeat(2, minmax(0, 1fr));
+				gap: 40px;
+			}
+			.itsc-case-section h3 {
+				margin-bottom: 18px;
+				font-size: 28px;
+			}
+			.itsc-case-list {
+				margin: 0;
+				padding-left: 20px;
+			}
+			.itsc-case-list li {
+				margin-bottom: 12px;
+			}
+			.itsc-case-list-implemented {
+				color: var(--ast-global-color-3);
+			}
+			.itsc-case-list-results {
+				color: var(--ast-global-color-2);
+				font-weight: 500;
+			}
+			@media (max-width: 767px) {
+				.itsc-case {
+					padding-top: 44px;
+					padding-bottom: 56px;
+				}
+				.itsc-case-screenshot-preview img {
+					max-height: 260px;
+				}
+				.itsc-case-lightbox {
+					padding: 16px;
+				}
+				.itsc-case-lightbox-content,
+				.itsc-case-lightbox-content img {
+					max-height: calc(100vh - 32px);
+				}
+				.itsc-case-grid {
+					grid-template-columns: 1fr;
+					gap: 28px;
+				}
+				.itsc-case-summary {
+					font-size: 18px;
+				}
+			}
+		</style>
+		<?php
+	}
+);
